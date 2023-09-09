38e7174275b9b76385b670125c4be338ad96cb7d
Wu Huijie <hjwu85@gmail.com>
2023-09-10 00:24:00 +0800

    Update dependencies

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -24,3 +24,6 @@ go install github.com/go-delve/delve/cmd/dlv@latest
 go install github.com/haya14busa/goplay@latest
 go install github.com/fatih/gomodifytags@latest
 go install github.com/josharian/impl@latest
+go install github.com/cweill/gotests/gotests@latest
+go install github.com/haya14busa/goplay/cmd/goplay@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
